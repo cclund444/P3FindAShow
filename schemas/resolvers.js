89edca5764d59987bd8234bd2e8dcac0a04ed928
cclund444 <87826114+cclund444@users.noreply.github.com>
2022-01-08 11:39:02 -0700

    update and mongo addition

--- schemas/resolvers.js
+++ schemas/resolvers.js
@@ -1,6 +1,6 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Comment } = require('../models')
-const { signToken } = require('../../utils/auth')
+const { signToken } = require('../utils/auth')
 
 
 const resolvers = {

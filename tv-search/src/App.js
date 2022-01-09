9cae93140c1e871860e147a5e959770c9bf6842d
AlejandroMrosas <alejandromrosas@outlook.com>
2022-01-09 15:08:05 -0700

    navbar working

--- tv-search/src/App.js
+++ tv-search/src/App.js
@@ -2,12 +2,12 @@ import { useState } from "react";
 import ReactDOM from 'react-dom';
 import Homepage from './pages/Homepage';
 import Navbar from './components/Navbar';
+import Routes from './Routes';
 
 function App() {
   return (
     <div>
-   <Navbar />
-   <Homepage />
+      <Routes />
    </div>
   );
 }
--- tv-search/src/Routes.js
+++ tv-search/src/Routes.js
@@ -1,13 +1,33 @@
-import React from "react";
-import { Route } from "react-router-dom";
-import Homepage from "./pages/Homepage"
+import React, { useState } from 'react';
+import NavTabs from './components/Navbar/index';
+import Home from './pages/Homepage';
+import Login from './pages/Login';
+// import Register from './pages/Register';
 
-export default function Routes() {
+function Routes() {
+  // Using useState, set the default value for currentPage to 'Home'
+  const [currentPage, handlePageChange] = useState('Home');
+
+  // The renderPage method uses a switch statement to render the appropriate current page
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'Login':
+        return <Login />;
+      // case 'Register':
+      //   return <Register />;
+      default:
+        return <Home />;
+    }
+  };
+
   return (
-    <Routes>
-      <Route exact path="/">
-        <Homepage />
-      </Route>
-    </Routes>
+    <div>
+      {/* Pass the state value and the setter as props to NavTabs */}
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      {/* Call the renderPage function passing in the currentPage */}
+      <div>{renderPage(currentPage)}</div>
+    </div>
   );
 }
+
+export default Routes;

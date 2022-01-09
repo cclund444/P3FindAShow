0e032e5a0683cbd37d69e1c21fb722df11f854e2
TylerMair <tyleramair@gmail.com>
2022-01-09 14:51:57 -0700

    heroku

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,6 @@ mongoose.connect(
     process.env.MONGODB_URI || 'mongodb://localhost:27017/findshow', {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        useFindAndModify: true
     }
 );
 

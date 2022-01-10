34f57cad5722b052c4c63178d65ce46686e31647
AlejandroMrosas <alejandromrosas@outlook.com>
2022-01-09 21:56:23 -0700

    signup form

--- tv-search/src/utils/auth.js
+++ tv-search/src/utils/auth.js
@@ -1,40 +1,35 @@
-import decode from 'jwt-decode';
+const jwt = require('jsonwebtoken');
 
-class AuthService {
-  getProfile() {
-    return decode(this.getToken());
-  }
+const secret = 'icannotkeepasecrettosavemylife'
+const expiration = '1h'
 
-  loggedIn() {
-    const token = this.getToken();
-    return !!token && !this.isTokenExpired(token); // handwaiving here
-  }
+module.exports = {
+    authMiddleware: function({ req }) {
+        let token = req.body.token || req.query.token || req.headers.authorization;
 
-  isTokenExpired(token) {
-    try {
-      const decoded = decode(token);
-      if (decoded.exp < Date.now() / 1000) {
-        return true;
-      } else return false;
-    } catch (err) {
-      return false;
+        if (req.headers.authorization) {
+            token = token
+            .split(' ')
+            .pop()
+            .trim();
+        }
+
+        if (!token) {
+            return req;
+        }
+
+        try {
+            const { data } = jwt.verify(token, secret, { maxAge: expiration });
+            req.user = data;
+        } catch {
+            console.log('Invalid Token');
+        }
+
+        return req;
+    },
+    signToken: function({ username, email, _id }){
+        const load = { username, email, _id };
+
+        return jwt.sign({ data: load }, secret, { expiresIn : expiration });
     }
-  }
-
-  getToken() {
-    return localStorage.getItem('id_token');
-  }
-
-  login(idToken) {
-    localStorage.setItem('id_token', idToken);
-
-    window.location.assign('/');
-  }
-
-  logout() {
-    localStorage.removeItem('id_token');
-    window.location.assign('/');
-  }
-}
-
-export default new AuthService();
+};

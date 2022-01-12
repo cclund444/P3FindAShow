74129f075f85b4c75ed9f78fe0017b23dc6abc72
TylerMair <tyleramair@gmail.com>
2022-01-12 13:40:46 -0700

    heroku

--- server.js
+++ server.js
@@ -26,11 +26,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 
-app.use(express.static(path.join(__dirname, '/client/build')));
+app.use(express.static(path.join(__dirname, '/build')));
 
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+    res.sendFile(path.join(__dirname, '/build/index.html'));
   });
 
 db.once('open', () => {
